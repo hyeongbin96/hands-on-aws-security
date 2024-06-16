0ea6606066f99b3683999b4765a3087bc14ff382
hyeongbin <gudqls1539@gmail.com>
2024-06-17 01:02:17 +0900

    function 사용법 예시 추가

--- terraform/advanced_terraform/3. Expression & Function/main.tf
+++ terraform/advanced_terraform/3. Expression & Function/main.tf
@@ -1,63 +1,15 @@
-variable "is_true" {
-  default = true
+variable "pucci" {
+  default = "pucci"
 }
 
-output "test" {
-  value = var.is_true ? 1 : 0
+output "function_startswith1" {
+  value = startswith(var.pucci, "pucci")
 }
 
-variable "settings" {
-  default = {
-    a = {
-      namespace = "123"
-      name      = "1"
-      value     = "124151"
-    }
-    b = {
-      namespace = "asdf"
-      name      = "asdf"
-      value     = "asdf"
-    }
-  }
+output "function_startswith2" {
+  value = startswith(var.pucci, "puccii")
 }
 
-resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "tf-test-name"
-  application         = "123"
-  solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.4 running Go 1.12.6"
-
-  dynamic "setting" {
-    for_each = var.settings
-    content {
-      namespace = setting.value["namespace"]
-      name      = setting.value["name"]
-      value     = setting.value["value"]
-    }
-  }
-}
-
-
-variable "test" {
-  default = {
-    rex     = "good"
-    vincent = "bad"
-    june    = "hmm"
-  }
-}
-
-output "test2" {
-  # value = { for k, v in var.test : k => v }
-  value = [for k, _ in var.test : k]
-}
-
-variable "rex" {
-  default = "rex"
-}
-
-output "test3" {
-  value = startswith(var.rex, "rex")
-}
-
-output "test4" {
+output "function_file" {
   value = file("main.tf")
 }
